0f958897e34ddcdc373084dd658f483c4b63de79
yyc009 <yyc009@ucsd.edu>
2013-05-29 02:07:07 -0700

    -added a precompute sql to define a new table for precomputing the important stuff

---
+++ XenCart/precompute.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE precompute(
+   customer_id  INTEGER REFERENCES users (id) NOT NULL,
+   product_id	INTEGER REFERENCES products (id) NOT NULL,
+   category	INTEGER REFERENCES categories (id) NOT NULL,
+   age		INTEGER,
+   state	INTEGER REFERENCES states (id) NOT NULL,
+   count	INTEGER,
+   month	INTEGER
+);
+
+INSERT INTO precompute
+SELECT customer_id, product_id, category, age, state, sum(quantity) AS count, month 
+FROM (sales JOIN users ON customer_id = users.id) JOIN products ON product_id = products.id 
+GROUP BY customer_id, age, state, product_id, category, month 
+ORDER BY customer_id ASC
